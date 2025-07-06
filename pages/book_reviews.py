9853d982f6fa60b9096ff3cd26e401ba144d5406
David Fellipe <82738833+lbanezz@users.noreply.github.com>
2025-07-05 23:26:09 -0300

    Update book_reviews.py

--- pages/book_reviews.py
+++ pages/book_reviews.py
@@ -15,7 +15,7 @@ df_reviews_f = df_reviews[df_reviews["book name"] == book]
 book_title = df_book["book title"].iloc[0]
 book_genre = df_book["genre"].iloc[0]
 book_price = f"${df_book['book price'].iloc[0]}"
-book_rating = df_book['rating'].iloc(0)
+book_rating = df_book['rating'].iloc[0]
 book_year = df_book['year of publication'].iloc[0]
 
 st.title(book_title)
@@ -23,4 +23,6 @@ st.subheader(book_genre)
 col1, col2, col3 = st.columns(3)
 col1.metric("Price", book_price)
 col2.metric("Rating", book_rating)
-col3.metric("year of publication", book_year)
+col3.metric("Year of publication", book_year)
+
+st.divider()
